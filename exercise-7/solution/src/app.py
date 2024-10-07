491dadd8b3ad9c8cc958cf22deb06cf98535cabe
Emile Fugulin <code@efugulin.com>
2024-10-07 15:53:03 -0400

    Exercise 7 Solution

---
+++ exercise-7/solution/src/app.py
@@ -0,0 +1,62 @@
+import os
+from gql import Client
+from gql.client import AsyncClientSession
+from gql.transport.aiohttp import AIOHTTPTransport
+
+from .queries import request_full
+from .gql import gql
+from .analyze import analyse
+from .finding import create_finding
+
+async def process_requests(client: AsyncClientSession, after: str = None):
+    query = gql(
+        """
+        query Requests($first: Int, $after: String) {
+            requests(first: $first, after: $after) {
+              nodes {
+                ...RequestFull
+              }
+              pageInfo {
+                endCursor
+                hasNextPage
+              }
+            }
+        }
+        """,
+        request_full,
+    )
+
+    after = None
+    i = 0
+    while True:
+        result = await client.execute(
+            query, variable_values={"first": 100, "after": after}
+        )
+
+        for request in result["requests"]["nodes"]:
+            finding = analyse(request)
+            if finding:
+                print(f"[-] Found reflected parameter(s) {finding.parameters} in request {finding.id}")
+                await create_finding(client, finding)
+
+        i += len(result["requests"]["nodes"])
+        print(f"[*] Processed {i} requests")
+
+        if result["requests"]["pageInfo"]["hasNextPage"]:
+            after = result["requests"]["pageInfo"]["endCursor"]
+        else:
+            break
+
+
+async def app():
+    auth = {"Authorization": f"Bearer {os.getenv('ACCESS_TOKEN')}"}
+
+    transport_queries = AIOHTTPTransport(
+        url= f"http://{os.getenv("CAIDO_ENDPOINT")}/graphql",
+        headers=auth,
+    )
+
+    async with Client(
+        transport=transport_queries, fetch_schema_from_transport=True, parse_results=True
+    ) as client_queries:
+        await process_requests(client_queries)
