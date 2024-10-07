4b4f14caf11683f56a4958392b7817a65c39a720
Emile Fugulin <code@efugulin.com>
2024-10-07 16:13:46 -0400

    Exercuse 4 solution

---
+++ exercise-4/solution/packages/backend/src/analyse.ts
@@ -0,0 +1,50 @@
+import type { Request, Response } from "caido:utils";
+
+import { notNullable } from "./utils";
+
+export type Finding = {
+  request: Request;
+  parameters: [string, string][];
+  dedupeKey: string;
+};
+
+export function analyse(request: Request, response: Response): Finding | null {
+  const query = request.getQuery();
+  if (!query) {
+    return null;
+  }
+
+  const parameters = Object.fromEntries(
+    query
+      .split("&")
+      .map<[string, string] | null>((part) => {
+        const i = part.indexOf("=");
+        if (i === -1) {
+          return null;
+        }
+        const key = part.slice(0, i);
+        const value = part.slice(i + 1);
+        return value ? [key, value] : null;
+      })
+      .filter(notNullable),
+  );
+
+  const responseRaw = response.getBody()?.toText() ?? "";
+
+  const reflectedParameters: [string, string][] = [];
+  for (const [key, value] of Object.entries(parameters)) {
+    if (responseRaw.includes(value)) {
+      reflectedParameters.push([key, value]);
+    }
+  }
+
+  if (reflectedParameters.length > 0) {
+    const dedupeKey = `${request.getMethod()}-${request.getHost()}-${request.getPath()}`;
+    return {
+      request,
+      parameters: reflectedParameters,
+      dedupeKey,
+    };
+  }
+  return null;
+}
---
+++ exercise-4/solution/packages/backend/src/index.ts
@@ -0,0 +1,25 @@
+import type { Request, Response } from "caido:utils";
+import type { SDK } from "caido:plugin";
+
+import { analyse } from "./analyse";
+import { createFinding } from "./finding";
+
+async function process_new(
+  sdk: SDK,
+  request: Request,
+  response: Response,
+): Promise<void> {
+  sdk.console.log(`New request ${request.getId()}`);
+
+  const finding = analyse(request, response);
+  if (finding) {
+    sdk.console.log(
+      `Found reflected parameter(s) ${finding.parameters} in request {finding.id}`,
+    );
+    await createFinding(sdk, finding);
+  }
+}
+
+export async function init(sdk: SDK) {
+  sdk.events.onInterceptResponse(process_new);
+}
