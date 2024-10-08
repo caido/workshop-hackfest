8a15c80632a9cea03655b4d69551cb3a264e92e0
Emile Fugulin <code@efugulin.com>
2024-10-07 22:03:01 -0400

    Exercise 2 starter

---
+++ exercise-2/starter/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "plugin-template",
+      fileName: (format) => "script.js",
+      formats: ["es"],
+    },
+    outDir: "dist",
+    minify: false,
+    rollupOptions: {
+      external: [/caido:.+/, "crypto"],
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+});
